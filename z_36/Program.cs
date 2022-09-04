d9ee5e8596f897869789de1b0c4ba01663b7dbe1
Katya <katya0897@yandex.ru>
2022-09-04 21:12:20 +0300

    resh

---
+++ z_36/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int num = Convert.ToInt32(Console.ReadLine()); 
+
+int RandomNum(int num, int min, int max)
+  {
+  int[] randomNum = new int[num];
+  int sumEl = 0;
+  Console.Write("Массив: ");
+
+    for (int i = 0; i <randomNum.Length; i++ )
+    {
+      randomNum[i] = new Random().Next(min, max);
+
+      Console.Write(randomNum[i] + " ");
+
+      if (i % 2 != 1)
+      {
+        sumEl+=randomNum[i];
+      }
+    }
+  return sumEl;
+  }
+int randomNum =  RandomNum(num, 1, 10);
+
+Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNum}");
